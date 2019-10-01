afa3b40253f05eef8e246912e3de30d6ead07191
sculouis <scu.louis@gmail.com>
2019-10-01 14:22:55 +0800

    add app get

--- express/server.js
+++ express/server.js
@@ -7,15 +7,27 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/datas', (req, res) => res.json({
+  data: "測試"
+}));
+
+router.post('/', (req, res) => res.json({
+  postBody: req.body
+}));
+
+app.get('/another', (req, res) => res.json({
+  route: "測試"
+}));
+
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
