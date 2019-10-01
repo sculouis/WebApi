69864c4e5b54ffb7d56fa3562d2c3d743e0688cf
sculouis <scu.louis@gmail.com>
2019-10-01 14:54:01 +0800

    api path

--- express/server.js
+++ express/server.js
@@ -21,13 +21,8 @@ router.post('/', (req, res) => res.json({
   postBody: req.body
 }));
 
-app.get('/another', (req, res) => res.json({
-  route: "測試"
-}));
-
-
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/api', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
