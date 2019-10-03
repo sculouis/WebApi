33d6548111de11ef5fb3f950626447376d4cf64a
sculouis <scu.louis@gmail.com>
2019-10-03 14:19:52 +0800

    remove sqlite3

--- express/server.js
+++ express/server.js
@@ -5,30 +5,6 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-
-db.serialize(function () {
-  db.run("CREATE TABLE IF NOT EXISTS  Items (info TEXT)");
-
-  var stmt = db.prepare("INSERT INTO Items VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-    stmt.run("測試" + i);
-  }
-  stmt.finalize();
-});
-
-db.allAsync = function (sql) {
-  var that = this;
-  return new Promise(function (resolve, reject) {
-      that.all(sql, function (err, rows) {
-          if (err)
-              reject(err);
-          else
-              resolve(rows);
-      });
-  });
-};
 
 router.get('/', (req, res) => {
   res.writeHead(200, {
@@ -38,9 +14,7 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/datas', (req, res) => {
-     let sql = "SELECT rowid AS id, info FROM Items"
-     return  db.allAsync(sql).then(rows => res.json(rows))
-    // res.json({test:"測試"})
+    res.json({test:"測試"})
 });
 
 router.post('/', (req, res) => res.json({
